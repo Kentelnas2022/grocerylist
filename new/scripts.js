fe2b8346e3330df352ea6888eb10ffff5d86a5c0
Kent Zorel B. Elnas <141716943+Kentelnas2022@users.noreply.github.com>
2024-09-10 16:50:13 +0800

    Add files via upload

---
+++ new/scripts.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const navMenu = document.getElementById('nav-menu');
+    
+    if (navMenu.classList.contains('show')) {
+        navMenu.classList.remove('show');
+    } else {
+        navMenu.classList.add('show');
+    }
+}
